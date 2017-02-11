ae28b9bbeba41624a5908fbf2ac1b3bb51b689dc
Antonin Delpeuch <antonin@delpeuch.eu>
2017-02-11 12:22:23 +0000

    Better monitoring

--- monitoring.py
+++ monitoring.py
@@ -2,7 +2,7 @@ import time
 
 class Monitoring(object):
     def __init__(self, redis_client):
-        self.req_rate_bucket_durations = [3600,60]
+        self.req_rate_bucket_durations = [24*3600,3600,60]
         self.r = redis_client
 
     def redis_bucket(self, duration):
@@ -26,11 +26,14 @@ class Monitoring(object):
             req_count = float(self.r.get(key+':req_count') or 0)
             query_count = float(self.r.get(key+':query_count') or 0)
             processing_time = float(self.r.get(key+':processing_time') or 0)
+            curtime = time.time()
+            time_since_bucket_started = curtime - duration*(curtime // duration)
             rates.append({
-                'request_rate': req_count / duration,
-                'query_rate': query_count / duration,
+                'request_rate': req_count / time_since_bucket_started,
+                'query_rate': query_count / time_since_bucket_started,
                 'processing_time_per_query': processing_time / query_count if query_count > 0 else None,
-                'measure_duration': duration,
+                'measure_duration': int(time_since_bucket_started),
+                'measure_duration_target': duration,
             })
         return rates
 
