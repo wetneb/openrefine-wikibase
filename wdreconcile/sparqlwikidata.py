af097500a5435e5756942b84c8b4394d33e2d34b
Antonin Delpeuch <antonin@delpeuch.eu>
2017-08-14 07:36:15 +0100

    Ditch SPARQLWrapper and use requests_cache for faster tests

--- wdreconcile/sparqlwikidata.py
+++ wdreconcile/sparqlwikidata.py
@@ -1,8 +1,5 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
+import requests
 
 def sparql_wikidata(query_string):
-    sparql_endpoint = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
-    sparql_endpoint.setQuery(query_string)
-    sparql_endpoint.setReturnFormat(JSON)
-    results = sparql_endpoint.query().convert()
+    results = requests.get('https://query.wikidata.org/sparql', {'query': query_string, 'format': 'json'}).json()
     return results['results']
