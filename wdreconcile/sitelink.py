796482c686331053baad55bddff8951ad2d3a5e1
Antonin Delpeuch <antonin@delpeuch.eu>
2017-08-15 21:50:21 +0100

    Add support for redirects in sitelinks. Thanks to WikidataFacts and Tpt.
    Closes #24.
    
    
    Former-commit-id: 71152bc9db93b70e807027431f178bfab417cb10

--- wdreconcile/sitelink.py
+++ wdreconcile/sitelink.py
@@ -1,6 +1,8 @@
 
 import re
-from urllib.parse import quote_plus
+import requests
+from urllib.parse import quote_plus, unquote_plus
+from collections import defaultdict
 
 from .sparqlwikidata import sparql_wikidata
 from .utils import to_q
@@ -21,6 +23,7 @@ class SitelinkFetcher(object):
         'wikinews',
         'wikiversity',
         'wiktionary',
+        'wikibooks',
     ])
 
     wikimedia_sites_without_capitalization = [
@@ -36,14 +39,55 @@ class SitelinkFetcher(object):
         self.ttl = 60*60 # one hour
 
     @classmethod
-    def safe_quote(cls, url):
+    def parse(cls, sitelink):
         """
-        Escapes an URL, but only if it hasn't been escaped yet!
+        Returns a parsed version of the wiki link
+
+        >>> SitelinkFetcher.parse('http://en.wikipedia.org/wiki/cluny')
+        ('en', 'wikipedia', 'Cluny')
+        >>> SitelinkFetcher.parse(' http://fr.wikipedia.org/wiki/Alan%20Turing ')
+        ('fr', 'wikipedia', 'Alan Turing')
+        >>> SitelinkFetcher.parse('https://de.wikiquote.org/wiki/Chelsea Manning')
+        ('de', 'wikiquote', 'Chelsea Manning')
+        >>> SitelinkFetcher.parse('https://de.wikiquote.org/wiki/Brüssel')
+        ('de', 'wikiquote', 'Brüssel')
+        >>> SitelinkFetcher.parse('https://de.wikiquote.org/wiki/Br%C3%BCssel')
+        ('de', 'wikiquote', 'Brüssel')
+        >>> SitelinkFetcher.parse('https://www.wikimedia.org/') is None
+        True
+        >>> SitelinkFetcher.parse('https://fr.wikipedia.org/wiki/') is None
+        True
+
         """
-        url_without_percent = url.replace('%','')
-        if quote_plus(url_without_percent) != url_without_percent:
-            return quote_plus(url)
-        return url
+        if not sitelink:
+            return None
+        sitelink = str(sitelink).strip()
+        match = cls.sitelink_regex.match(sitelink)
+        if not match:
+            return None
+
+        title = match.group(3).replace('%20', ' ').replace('_', ' ')
+        title = unquote_plus(title)
+
+        wiki = match.group(2)
+        if wiki not in cls.wikimedia_sites_without_capitalization:
+            title = title[0].upper()+title[1:]
+
+        lang_code = match.group(1)
+        return (lang_code, wiki, title)
+
+    @classmethod
+    def wiki_id(cls, lang_code, domain):
+        """
+        Returns the wiki name in the API given the language code and domain.
+
+        >>> SitelinkFetcher.wiki_id('en', 'wikipedia')
+        'enwiki'
+        >>> SitelinkFetcher.wiki_id('de', 'wikibooks')
+        'dewikibooks'
+        """
+        domain_fixed = 'wiki' if domain == 'wikipedia' else domain
+        return lang_code + domain_fixed
 
     @classmethod
     def normalize(cls, sitelink):
@@ -66,22 +110,75 @@ class SitelinkFetcher(object):
         >>> SitelinkFetcher.normalize('https://fr.wikipedia.org/wiki/') is None
         True
         """
-        if not sitelink:
+        parsed = cls.parse(sitelink)
+        if not parsed:
             return None
-        sitelink = str(sitelink).strip()
-        match = cls.sitelink_regex.match(sitelink)
-        if not match:
-            return None
+        lang_code, wiki, title = parsed
 
-        cleaned_title = cls.safe_quote(match.group(3).replace('%20', ' ').replace(' ', '_'))
+        cleaned_title = quote_plus(title.replace(' ', '_'))
 
-        wiki = match.group(2)
-        if wiki not in cls.wikimedia_sites_without_capitalization:
-            cleaned_title = cleaned_title[0].upper()+cleaned_title[1:]
         return 'https://{}.{}.org/wiki/{}'.format(
-            match.group(1),
+            lang_code,
             wiki,
             cleaned_title)
+
+    @classmethod
+    def get_qids_via_api(cls, wiki_id, titles):
+        """
+        Given a wiki code and a list of titles for that wiki,
+        return the list of qids (or Nones) corresponding to these
+        titles
+        """
+        results = [None] * len(titles)
+        title_string = '|'.join(titles)
+        params =  {'action': 'wbgetentities',
+                 'props': 'sitelinks',
+                 'sites': wiki_id,
+                 'titles': title_string,
+                 'format': 'json'}
+        try:
+            r = requests.get('https://www.wikidata.org/w/api.php', params=params)
+            for qid, item in r.json().get('entities', {}).items():
+                own_title = item.get('sitelinks', {}).get(wiki_id, {}).get('title')
+                if own_title:
+                    idx = titles.index(own_title)
+                    results[idx] = qid
+        except requests.exceptions.RequestException as e:
+            print(e)
+        except ValueError as e:
+            print("Unexpected error in get_qids_via_api")
+            raise e
+        return results
+
+    @classmethod
+    def resolve_redirects_for_titles(cls, lang_code, wiki, titles):
+        """
+        >>> SitelinkFetcher.resolve_redirects_for_titles('en','wikipedia', ['Knuth-Bendix','Lowendal', 'Paris'])
+        ['Knuth–Bendix completion algorithm', 'Ulrich Frédéric Woldemar, Comte de Lowendal', 'Paris']
+        """
+        try:
+            r = requests.get('https://{}.{}.org/w/api.php'.format(lang_code, wiki),
+                params={
+                    'action': 'query',
+                    'format': 'json',
+                    'redirects': '1',
+                    'titles': '|'.join(titles),
+                })
+            response = r.json()['query'].get('redirects', [])
+            redirect_map = {
+                redirect['from']:redirect['to']
+                for redirect in response
+            }
+
+            results = []
+            for title in titles:
+                while title in redirect_map:
+                    title = redirect_map[title]
+                results.append(title)
+            return results
+
+        except requests.exceptions.RequestException as e:
+            raise e
 
     @classmethod
     def get_qids(cls, sitelinks):
@@ -90,40 +187,33 @@ class SitelinkFetcher(object):
         the Qids they are associated to (or None if they are invalid,
         or not linked yet).
 
-        >>> SitelinkFetcher.get_qids(['https://de.wikipedia.org/wiki/Chelsea_Manning', None])
-        ['Q298423', None]
+        >>> SitelinkFetcher.get_qids(['https://de.wikipedia.org/wiki/Chelsea_Manning', 'https://en.wikipedia.org/wiki/Knuth-Bendix'])
+        ['Q298423', 'Q2835803']
         >>> SitelinkFetcher.get_qids([None, None]) # no request made
         [None, None]
         """
-        result = [None] * len(sitelinks)
+        qids = [None] * len(sitelinks)
 
-        if all(sitelink is None for sitelink in sitelinks):
-            return result
+        # Group sitelinks by wiki
+        by_wiki = defaultdict(list)
+        for idx, sitelink in enumerate(sitelinks):
+            parsed = cls.parse(sitelink)
+            if parsed:
+                lang_code, wiki, title = parsed
+                by_wiki[(lang_code, wiki)].append((idx, title))
 
-        sitelink_list = ' '.join(
-           '<%s>' % sitelink
-            for sitelink in sitelinks
-            if sitelink
-        )
+        for (lang_code, wiki), titles in by_wiki.items():
+            # Resolve redirects
+            redirected_titles = cls.resolve_redirects_for_titles(lang_code, wiki, [title for _, title in titles])
 
-        query = """
-        PREFIX schema: <http://schema.org/>
-        SELECT ?item ?sitelink WHERE {
-        ?sitelink schema:about ?item .
-        VALUES ?sitelink { %s }
-        }
-        """ % sitelink_list
+            # Resolve qids
+            wiki_id = cls.wiki_id(lang_code, wiki)
+            current_qids = cls.get_qids_via_api(wiki_id, redirected_titles)
 
-        for binding in sparql_wikidata(query)['bindings']:
-            qid = to_q(binding["item"]["value"])
-            sitelink = binding["sitelink"]["value"]
-            try:
-                idx = sitelinks.index(sitelink)
-                result[idx] = qid
-            except ValueError:
-                print('Normalization error for sitelink: "{}"'.format(sitelink))
+            for new_idx, (orig_idx, title) in enumerate(titles):
+                qids[orig_idx] = current_qids[new_idx]
 
-        return result
+        return qids
 
     def _key_for_sitelink(self, sitelink):
         return ':'.join([self.prefix, sitelink])
