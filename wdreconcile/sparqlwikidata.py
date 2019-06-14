6d8c7612fa3f6bb969c507f55ab20370e23cffc1
Antonin Delpeuch <antonin@delpeuch.eu>
2019-06-14 09:00:27 +0100

    Fix API and SPARQL queries

--- wdreconcile/sparqlwikidata.py
+++ wdreconcile/sparqlwikidata.py
@@ -2,5 +2,9 @@ import requests
 import config
 
 def sparql_wikidata(query_string):
-    results = requests.get(config.wikibase_sparql_endpoint, {'query': query_string, 'format': 'json'}).json()
+    results = requests.get(
+        config.wikibase_sparql_endpoint,
+        {'query': query_string, 'format': 'json'},
+        headers={'User-Agent': config.user_agent}
+        ).json()
     return results['results']
