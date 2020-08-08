1329fb0c1393e47f39495351234302068e90d0ea
Antonin Delpeuch <antonin@delpeuch.eu>
2020-08-08 15:49:01 +0200

    Simplify external-id query for Wikibases
    Former-commit-id: 42d7edb7e141094abb213d11b68de83ecf0be782

--- config_docker.py
+++ config_docker.py
@@ -150,7 +150,7 @@ SELECT ?child WHERE { ?child wdt:P279* wd:$qid }
 
 # Sparql query used to fetch all the properties which store unique identifiers
 sparql_query_to_fetch_unique_id_properties = """
-SELECT ?pid WHERE { ?pid wdt:P31/wdt:P279* wd:Q19847637 }
+SELECT ?pid WHRE { ?pid wikibase:propertyType wikibase:ExternalId }
 """
 
 # Sparql query used to propose properties to fetch for items of a given class.
